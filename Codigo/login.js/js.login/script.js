5f938b5d7c012dea994ca010204c59c59510b23d
Marlene Moraes <marlenevmoraes@outlook.com>
2021-05-21 06:44:43 -0300

    SUPPORT: unificação de pastas login js #22

---
+++ Codigo/login.js/js.login/script.js
@@ -0,0 +1,16 @@
+
+
+ function validate(){
+
+ var email = document.getElementById("email").value;
+
+
+ var password = document.getElementById("password").value;
+
+ if ( email == "cashfix@gmail.com" && password == "123456")
+     {window.location = "pagina interna.html";}
+
+ else{
+          alert("VERIFIQUE USUÁRIO OU SENHA!");}
+ }
+
