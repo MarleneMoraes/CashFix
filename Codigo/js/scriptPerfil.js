5006cb61d6fe29bde8a6055e1efdb318aab360da
Marlene Moraes <marlenevmoraes@outlook.com>
2021-07-06 12:28:40 -0300

    FRONT: update perfil page #29

---
+++ Codigo/js/scriptPerfil.js
@@ -0,0 +1,44 @@
+//Variáveis
+
+const nome = document.querySelector('.nome')
+let labelNome = document.querySelector('#labelNome')
+let validaNome = false
+
+const nome = document.querySelector('.sobrenome')
+let labelSobrenome = document.querySelector('#labelSobrenome')
+let validaSobrenome = false
+
+let email = document.querySelector('.email')
+let labelEmail = document.querySelector('#labelEmail')
+let validaEmail = false
+
+let renda = document.querySelector('.renda')
+let labelRenda = document.querySelector('#labelRenda')
+let validaRenda = false
+
+let telefone = document.querySelector('.telefone')
+let labelTelefone = document.querySelector('#labelTelefone')
+let validaTelefone = false
+
+let endereco = document.querySelector('.endereco')
+let labelEndereco = document.querySelector('#labelEndereco')
+let validaEndereco = false
+
+let numero = document.querySelector('.numero')
+let labelNumero = document.querySelector('#labelNumero')
+let validaNumero = false
+
+let cidade = document.querySelector('.cidade')
+let labelCidade = document.querySelector('#labelCidade')
+let validaCidade = false
+
+let estado = document.querySelector('.estado')
+let labelEstado = document.querySelector('#labelEstado')
+let validaEstado = false
+
+let cep = document.querySelector('.cep')
+let labelCep = document.querySelector('#labelCep')
+let validaCep = false
+
+let mensagemErro = document.querySelector('#mensagemErro')
+let mensagemSucesso = document.querySelector('#mensagemSucesso')
