2907394a33f4e1dddfc7b659f39a681520a6ca59
Weslley OlI’s <83042218+weslleyolis@users.noreply.github.com>
2021-07-07 20:16:19 -0300

    FRONT: página Metas atribuída ao site;

---
+++ Codigo/js/scriptmetas.js
@@ -0,0 +1,23 @@
+var btn = document.getElementById('nova-meta');
+var container1 = document.querySelector('.container1');
+
+btn.addEventListener('click', function() {
+    
+  if(container1.style.display === 'block') {
+      container1.style.display = 'none';
+  } else {
+      container1.style.display = 'block';
+  }
+});
+
+var btn = document.getElementById('editar-meta');
+var container2 = document.querySelector('.container2');
+
+btn.addEventListener('click', function() {
+    
+  if(container2.style.display === 'block') {
+      container2.style.display = 'none';
+  } else {
+      container2.style.display = 'block';
+  }
+});
