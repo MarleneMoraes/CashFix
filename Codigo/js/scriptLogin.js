3b715188949b966138be46a87ee3b8da2a804b67
Marlene Moraes <marlenevmoraes@outlook.com>
2021-07-08 07:15:34 -0300

    FRONT: update localStorage #28

--- Codigo/js/scriptLogin.js
+++ Codigo/js/scriptLogin.js
@@ -43,7 +43,7 @@ btnEntrar.addEventListener('click', () => {
             }
             
             setTimeout(() => {
-                window.location.href = 'pagInternaBT.html'
+                window.location.href = 'pagInterna.html'
               }, 2000)
         }
     })
