627f4161a1fef22adf5c87df4fc4ea8e5b0813a9
Marlene Moraes <marlenevmoraes@outlook.com>
2021-07-08 17:32:27 -0300

    FRONT: update cartoes #26

---
+++ Codigo/js/scriptCartoes.js
@@ -0,0 +1,44 @@
+/*aparecer mensagem do usuário logado
+let usuario = document.querySelector('#usuario')
+
+//Saudação
+usuario.setAttribute('style', 'color:white')  
+usuario.innerHTML = `<p>Olá ${nomeCadastrado}! Sua sessão expirará em</p>`;
+
+*/
+
+/* clicar no botão + e aparecer a sessão 
+
+let cartao = document.querySelector('#sessaoCadastro')
+let botaoMais = document.querySelector('#cartaoAdd')
+let tipoCartao = document.querySelector('#tipoCartao')
+
+function sessaoCadastrar () {
+     cartao.setAttribute ('style', 'opacity:visible')
+}
+
+if (cartao.getAttribute('style') == 'opacity:hidden;') {
+       
+    } 
+
+
+
+    
+
+
+tipoCartao.addEventListener()*/
+
+/*var btn = document.getElementById('nova-meta');
+var container1 = document.querySelector('.container1');
+
+btn.addEventListener('click', function() {
+    
+  if(container1.style.display === 'block') {
+      container1.style.display = 'none';
+  } else {
+      container1.style.display = 'block';
+  }
+});
+
+
+visibility: visible;*/
