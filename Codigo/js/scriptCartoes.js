8c8138d6e4baedd6343933d8642b9e2f743fd027
Marlene Moraes <marlenevmoraes@outlook.com>
2021-07-10 02:00:43 -0300

    FRONT: paginaCartoes #26 #28

--- Codigo/js/scriptCartoes.js
+++ Codigo/js/scriptCartoes.js
@@ -1,71 +1,66 @@
-let botaoMais = document.querySelector('#cartaoAdd')
-let cadastroCartao = document.querySelector('#sessaoCadastro')
+let botaoMais = document.getElementById('cartaoAdd');
+
+let cadastroCartao = document.querySelector('#sessaoCadastro');
 
 const tipoCartao = document.querySelector('#tipoCartao')
 const tituloExtrato = document.querySelector('#tituloExtrato')
 
-botaoMais.addEventListener("click", function () {
-    if (cadastroCartao.style.display === "none") {
-        cadastroCartao.style.display = "block"
-    } else {
-        cadastroCartao.style.display = "none"
-    }
-});
-
-//const select = document.querySelector('select');
-//const h4 = document.querySelector('h4');
-
-tipoCartao.addEventListener('change', (option) => {
-    tituloExtrato.textContent = option.target.value
+botaoMais.addEventListener("click", function (e) {
+    e.preventDefault();
+    cadastroCartao.style.display = "block";
 });
 
 //Armazenamento de dados
-let nomeCartao = document.querySelector('#nome')
-let limiteCartao = document.querySelector('#limite')
-let vencimentoCartao = document.querySelector('#vencimento')
-let btnSalvarCartao = document.querySelector('#salvarCartao')
-let cartoesSalvos = document.querySelector('#idCartoesSalvos')
+const nomeCartao = document.querySelector('#nome')
+const limiteCartao = document.querySelector('#limite')
+const vencimentoCartao = document.querySelector('#vencimento')
+const btnSalvarCartao = document.getElementById('salvarCartao')
+const cartoesSalvos = document.getElementById('cartoesSalvos')
 
 
 //Sessão Cartões Salvos
+function mostrarCartoes() {
+    let listaDeCartoes = JSON.parse(localStorage.getItem('cartoesSalvos') || '[]')
 
-async function mostrarCartoes() {
-    let resposta = await JSON.parse(localStorage.getItem('cartoesSalvos') || '[]')
+    cartoesSalvos.innerHTML = '';
 
-    for (let valor of resposta) {
+    listaDeCartoes.forEach(cartao => {
         cartoesSalvos.innerHTML += `
             <div class="cartao posicaoCartao">
-            <div id="cartao">
-                    <h4>${valor.cartao}</h4>
-                    <input type="text" value="R$ ${valor.limite}">
-                    <p class="data"><small>${valor.vencimento}</small></p>
+                <div id="cartao">
+                    <h4>${cartao.nome}</h4>
+                    <input type="text" value="R$ ${cartao.limite}" disabled>
+                    <p class="data"><small>${cartao.vencimento}</small></p>
                 </div>
             </div>
         `
-    }
+    });
 }
 
-mostrarCartoes()
+
+
+function formataData(data) {
+    return data.split('-').reverse().join('/');
+}
+
 
 btnSalvarCartao.addEventListener('click', () => {
-
-    async function adicionarCartao() {
-        let resposta = await JSON.parse(localStorage.getItem('cartoesSalvos') || '[]')
-
-        let valoresCartao = {
-            cartao: nomeCartao.value,
-            limite: limiteCartao.value,
-            vencimento: vencimentoCartao.value
-        }
-
-        resposta.push(valoresCartao)
-        await localStorage.setItem('cartoesSalvos', JSON.stringify(resposta))
-
-        mostrarCartoes()
+    let novoCartao = {
+        nome: nomeCartao.value,
+        limite: limiteCartao.value,
+        vencimento: formataData(vencimentoCartao.value)
     }
 
-    adicionarCartao()
+    adicionarCartao(novoCartao);
+    mostrarCartoes();
 })
 
+function adicionarCartao(novoCartao) {
+    let listaDeCartoes = JSON.parse(localStorage.getItem('cartoesSalvos') || '[]')
+
+    listaDeCartoes.push(novoCartao)
+    localStorage.setItem('cartoesSalvos', JSON.stringify(listaDeCartoes));
+}
 
 
+mostrarCartoes()
